b6055f007754ae675c344085ad953a613bc25593
IlkinAkhmed <tu7mp4w99@code.edu.az>
2023-11-09 01:05:04 +0400

    isim gucum qurtarib eliynen ehmedin sehvini duzeldirem

--- assets/script.js
+++ assets/script.js
@@ -6,20 +6,20 @@
 const barIcon = document.getElementById('navbar_bar_icon')
 const closeIcon = document.getElementById('navbar_bar_close_icon')
 const clickBar = document.querySelector('.click_bar')
-const blur = document.querySelector('.page_blur')
+const blurPage = document.querySelector('.page_blur')
 
 barIcon.addEventListener('click', () => {
-    clickBar.style.opacity = '1'
+    clickBar.style.display = 'block'
     barIcon.style.display = 'none'
     closeIcon.style.display = 'block'
-    blur.style.display = 'block'
+    blurPage.style.display = 'block'
 
 })
 closeIcon.addEventListener('click', () => {
-    clickBar.style.opacity = '0'
+    clickBar.style.display = 'none'
     barIcon.style.display = 'block'
     closeIcon.style.display = 'none'
-    blur.style.display = 'none'
+    blurPage.style.display = 'none'
 
 })
 // hamburger clicked open navbar end
